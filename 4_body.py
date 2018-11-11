1e971862a507a2ac660931b7085300d1a1230c1d
Francesco Di Natale <dinatale3@llnl.gov>
2018-11-11 11:58:34 -0800

    Correction of Mercury and Venus distances.

--- 4_body.py
+++ 4_body.py
@@ -22,12 +22,12 @@ class TwobodyWindow(arcade.Window):
         self.earth = PointMass("Earth", Vector2D(0.0, 149.6e9),
                                Vector2D(29.78e3, 0.0),
                                695.508e6, 5.972e24, arcade.color.YELLOW)
-        self.mercury = PointMass("Mercury", Vector2D(0.0, -35.98e6),
+        self.mercury = PointMass("Mercury", Vector2D(0.0, 57.91e9),
                                  Vector2D(48e3, 0.0),
-                                 695.508e6, 3.285e23, arcade.color.YELLOW)
-        self.venus = PointMass("Venus", Vector2D(0.0, -67.24e6),
+                                 695.508e6, 3.285e23, arcade.color.RED)
+        self.venus = PointMass("Venus", Vector2D(0.0, 108.2e9),
                                Vector2D(35e3, 0.0),
-                               695.508e6, 4.867e24, arcade.color.YELLOW)
+                               695.508e6, 4.867e24, arcade.color.WHITE)
 
         self.sun.print_summary()
         self.earth.print_summary()
